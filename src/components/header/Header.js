f363c143ceb26fad90fd555acac1af95063a7584
t4tiana <tianarenees@gmail.com>
2024-08-19 21:36:47 -0400

    .

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,6 @@ import {
   workExperiences,
   skillsSection,
   recentProjects,
-  blogSection,
   talkSection,
   achievementSection,
   resumeSection
@@ -20,7 +19,6 @@ function Header() {
   const viewOpenSource = recentProjects.display;
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
-  const viewBlog = blogSection.display;
   const viewTalks = talkSection.display;
   const viewResume = resumeSection.display;
 
@@ -59,11 +57,6 @@ function Header() {
           {viewAchievement && (
             <li>
               <a href="#achievements">Achievements</a>
-            </li>
-          )}
-          {viewBlog && (
-            <li>
-              <a href="#blogs">Blogs</a>
             </li>
           )}
           {viewTalks && (
--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Tiana Silva",
+  title: "Hi there, I'm Tiana",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A Full Stack Developer 🚀 with experience building web applications using Java / C# / JavaScript / React / Vue.js and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
@@ -367,7 +367,6 @@ export {
   recentProjects,
   bigProjects,
   achievementSection,
-  blogSection,
   talkSection,
   podcastSection,
   contactInfo,
