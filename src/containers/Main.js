8dea3df4bd713447d5f9fa120d2026cba6176d55
t4tiana <tianarenees@gmail.com>
2024-08-23 08:07:08 -0400

    removed automatic deploy. try to fixcname being reset

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,7 +4,7 @@ import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
 import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
-import Projects from "./projects/Projects";
+// import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
 import Footer from "../components/footer/Footer";
@@ -51,7 +51,7 @@ const Main = () => {
             <Greeting />
             <Skills />
             <StartupProject />
-            <Projects />
+            {/* <Projects /> */}
             <StackProgress />
             <WorkExperience />
             <Achievement />
